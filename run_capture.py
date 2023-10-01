e2df33a087d7374eab2a1d95b119101fa4497f94
Patiphon Loetsuthakun <36232545+Phon1209@users.noreply.github.com>
2023-10-01 23:31:56 +0700

    Update calling procedure

--- run_capture.py
+++ run_capture.py
@@ -16,14 +16,14 @@ time_slice = input("time per file(second): ")
 file_count = input("file count: ")
 
 
-setup_command = f"\"sudo bash ./setup.sh {channel} {width} {mac}\""
-run_command = f"\"sudo bash ./capture.sh {folder} \'{description}\' {time_slice} {file_count}\""
+setup_command = f"sudo bash ./setup.sh {channel} {width} {mac}"
+run_command = f"sudo bash ./capture.sh {folder} '{description}' {time_slice} {file_count}"
 
 for device in devices:
   ip = f"{device['user']}@{device['ip']}"
 
   print("Executing: ", " ".join(["ssh", ip, setup_command]))
-  subprocess.call(["ssh", ip, setup_command])
+  subprocess.call(["ssh", ip, setup_command], shell=True)
 
   print("Executing: ", " ".join(["ssh", ip, run_command]))
-  subprocess.Popen(["ssh", ip, run_command])
+  subprocess.Popen(["ssh", ip, run_command], shell=True)
